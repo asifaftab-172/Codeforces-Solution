11bd6a1506d5f163e73cbb053628e295df8cd6e6
asifaftab-172 <67755288+asifaftab-172@users.noreply.github.com>
2024-04-16 15:24:55 +0600

    Create 1041A.cpp
    Heist

---
+++ 1041A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+    int n;
+    cin>>n;
+    int arr[n];
+    
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    cout<<((arr[n-1]-arr[0])-n+1)<<endl;
+}
