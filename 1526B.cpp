c826855f460e0b19fb1fc511e07a28ae0d863c5a
asifaftab-172 <67755288+asifaftab-172@users.noreply.github.com>
2024-07-26 02:16:18 +0600

    I Hate 1111

---
+++ 1526B.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int x;
+        cin>>x;
+        cout<<((x/111)>=x%11?"YES":"NO")<<endl;
+        
+    }
+}
